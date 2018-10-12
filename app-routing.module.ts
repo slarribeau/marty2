920ed54e644f66400ece76fd352b3ad7b219e1d8
slarribeau <slarribeau@gmail.com>
2018-10-12 15:01:45 -0700

    Its funky, but trying out a way to use child routes so that I can display something twice

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -2,12 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {StandingsComponent} from './standings/standings.component';
 import {Test2Component} from './test2/test2.component';
+import { AppComponent } from './app.component';
+//import {AppComponent} from './app.component'
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/metrics', pathMatch: 'full'},
   { path: 'subscriptions', component: Test2Component },
-  { path: 'subscriptions/:id/:date', component: StandingsComponent },
+  //{ path: 'subscriptions/:id/:date', component: StandingsComponent },
+
+  { path: 'subscriptions/:id/:date', 
+    component: AppComponent,
+    children: [
+      {
+        path:'',
+         component: StandingsComponent,
+         outlet: 'child1'
+      },
+      {
+         path:'',
+         component: StandingsComponent,
+         outlet: 'child2'
+      }
+    ]
+  },
   { path: 'metrics', component: Test2Component},
   { path: '**', component: Test2Component }
 
